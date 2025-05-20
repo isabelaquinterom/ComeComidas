cb1e6228866bb29cb5ad5680000d18584e461e1a
Isabela Quintero Murillo <isaquinteromurillo1304@gmail.com>
2025-05-20 16:33:53 -0500

    Agregando paquete models

---
+++ src/autonoma/ComeComidas/models/Comida.java
@@ -0,0 +1,10 @@
+package autonoma.ComeComidas.models;
+
+import autonoma.ComeComida.models.Elemento;
+
+public class Comida extends Elemento {
+    public Comida(int x) {
+        super(x, 2, "/imagenes/comida.png"); // velocidad más lenta
+    }
+}
+
