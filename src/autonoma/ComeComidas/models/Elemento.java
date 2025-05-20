b5f5ccb6020795c4283008758d1848d6969842d9
Isabela Quintero Murillo <isaquinteromurillo1304@gmail.com>
2025-05-20 16:42:48 -0500

    agregando clase Elemento

---
+++ src/autonoma/ComeComidas/models/Elemento.java
@@ -0,0 +1,34 @@
+package autonoma.ComeComidas.models;
+
+import autonoma.ComeComida.models.JuegoInterface;
+import java.awt.*;
+
+import javax.swing.ImageIcon;
+
+public abstract class Elemento implements JuegoInterface {
+    protected int x, y, velocidad;
+    protected Image imagen;
+
+    public Elemento(int x, int velocidad, String ruta) {
+        this.x = x;
+        this.y = 0;
+        this.velocidad = velocidad;
+        this.imagen = new ImageIcon(getClass().getResource(ruta)).getImage();
+    }
+
+    public void mover() {
+        y += velocidad;
+    }
+
+    public void dibujar(Graphics g) {
+        g.drawImage(imagen, x, y, 50, 50, null);
+    }
+
+    public boolean contiene(int mx, int my) {
+        return mx >= x && mx <= x + 50 && my >= y && my <= y + 50;
+    }
+
+    public int getY() {
+        return y;
+    }
+} 
