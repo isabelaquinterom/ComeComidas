03c070c1eb30cc0ceeb21716353a7af2b9649076
Isabela Quintero Murillo <isaquinteromurillo1304@gmail.com>
2025-05-20 16:37:15 -0500

    Agregando clase ComidaPizza

---
+++ src/autonoma/ComeComidas/models/ComidaPizza.java
@@ -0,0 +1,11 @@
+package autonoma.ComeComidas.models;
+
+import autonoma.ComeComidas.models.Comida;
+
+public class ComidaPizza extends Comida {
+    public ComidaPizza(int x) {
+        super(x);
+        this.imagen = new javax.swing.ImageIcon(getClass().getResource("/imagenes/pizza.png")).getImage();
+    }
+}
+  
