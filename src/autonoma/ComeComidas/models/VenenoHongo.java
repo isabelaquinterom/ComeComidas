06679df14c47281f3c4782de3ac5eade5baccce8
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-21 08:31:47 -0500

    clase venenohongo agregada

---
+++ src/autonoma/ComeComidas/models/VenenoHongo.java
@@ -0,0 +1,8 @@
+package autonoma.ComeComida.models;
+
+public class VenenoHongo extends Veneno {
+    public VenenoHongo(int x) {
+        super(x);
+        this.imagen = new javax.swing.ImageIcon(getClass().getResource("/imagenes/hongo.png")).getImage();
+    }
+}
