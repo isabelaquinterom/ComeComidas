e2eb718b3ece94b379f8621299d44e5da052d04c
Juan Esteban Giraldo Betancur <juanesgiraldobetancur@gmail.com>
2025-05-21 08:34:56 -0500

    agregando clase veneno

---
+++ src/autonoma/ComeComidas/models/Veneno.java
@@ -0,0 +1,9 @@
+package autonoma.ComeComida.models;
+
+import autonoma.ComeComidas.models.Elemento;
+
+public class Veneno extends Elemento {
+    public Veneno(int x) {
+        super(x, 1, "/imagenes/veneno.png"); // velocidad más lenta
+    }
+}
