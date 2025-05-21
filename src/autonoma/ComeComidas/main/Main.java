e3519059fbbb8ba47cb14312ae963fc12052cba5
Isabela Quintero Murillo <isaquinteromurillo1304@gmail.com>
2025-05-21 10:59:04 -0500

    agregando clase organizada

--- src/autonoma/ComeComidas/main/Main.java
+++ src/autonoma/ComeComidas/main/Main.java
@@ -1,10 +1,17 @@
+// Paquete principal del proyecto, contiene la clase de entrada al programa
 package autonoma.ComeComidas.main;
 
+// Se importa la clase JuegoFrame desde el paquete de vistas (Views)
 import autonoma.ComeComidas.Views.JuegoFrame;
 
+// Clase principal del programa
 public class Main {
+
+    // Método main: punto de entrada del programa
     public static void main(String[] args) {
+        // Se crea una nueva instancia de JuegoFrame, lo que lanza la interfaz del juego
         new JuegoFrame();
     }
 }
+ 
 
